455f2f28e4398f9e39958fab1e83854edf6c9ab8
adam.w <a.wang@groupeonepoint.com>
2025-07-09 11:43:15 +1000

    add 2 floor solution

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env python3
+"""
+This module contains a type-annotated function for computing the floor of a float.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a float number.
+
+    Args:
+        n (float): The float number to compute the floor of.
+
+    Returns:
+        int: The largest integer less than or equal to n.
+    """
+    return math.floor(n)
